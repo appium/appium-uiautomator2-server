4697c6b0de3f23ea294a472128eb28980611a0f4
Mykola Mokhnach <nick+1@wire.com>
2018-05-03 21:49:43 +0200

    Restore class package

---
+++ app/src/test/java/io/appium/uiautomator2/server/test/AppiumUiAutomator2Server.java
@@ -0,0 +1,36 @@
+package io.appium.uiautomator2.server.test;
+
+import android.os.SystemClock;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.appium.uiautomator2.common.exceptions.SessionRemovedException;
+import io.appium.uiautomator2.server.ServerInstrumentation;
+import io.appium.uiautomator2.utils.Logger;
+
+@RunWith(AndroidJUnit4.class)
+public class AppiumUiAutomator2Server {
+    private static ServerInstrumentation serverInstrumentation;
+
+    /**
+     * Starts the server on the device
+     */
+    @Test
+    public void startServer() {
+        if (serverInstrumentation == null) {
+            serverInstrumentation = ServerInstrumentation.getInstance();
+            Logger.info("[AppiumUiAutomator2Server]", " Starting Server");
+            try {
+                while (!serverInstrumentation.isServerStopped()) {
+                    SystemClock.sleep(1000);
+                    serverInstrumentation.startServer();
+                }
+            } catch (SessionRemovedException e) {
+                //Ignoring SessionRemovedException
+            }
+        }
+    }
+}
+
