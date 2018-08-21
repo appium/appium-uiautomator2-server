7147a3a29858a2847ff78b8cc0ad30bedebff15a
Mykola Mokhnach <nick+1@wire.com>
2018-08-21 21:39:36 +0200

    Apply small refactoring and add license headers

--- app/src/main/java/io/appium/uiautomator2/model/KnownElements.java
+++ app/src/main/java/io/appium/uiautomator2/model/KnownElements.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.appium.uiautomator2.model;
 
 import android.support.annotation.Nullable;
@@ -8,6 +24,7 @@ import java.util.Map;
 
 import io.appium.uiautomator2.common.exceptions.StaleElementReferenceException;
 import io.appium.uiautomator2.model.internal.CustomUiDevice;
+import io.appium.uiautomator2.utils.Logger;
 import io.appium.uiautomator2.utils.NodeInfoList;
 
 import static io.appium.uiautomator2.utils.Device.getAndroidElement;
@@ -18,6 +35,7 @@ import static io.appium.uiautomator2.utils.LocationHelpers.toSelector;
 public class KnownElements {
     private static Map<String, AndroidElement> cache = new HashMap<>();
 
+    @Nullable
     private static String getCacheKey(AndroidElement element) {
         for (Map.Entry<String, AndroidElement> entry : cache.entrySet()) {
             if (entry.getValue().equals(element)) {
@@ -27,11 +45,9 @@ public class KnownElements {
         return null;
     }
 
+    @Nullable
     public static String getIdOfElement(AndroidElement element) {
-        if (cache.containsValue(element)) {
-            return getCacheKey(element);
-        }
-        return null;
+        return cache.containsValue(element) ? getCacheKey(element) : null;
     }
 
     @Nullable
@@ -67,7 +83,8 @@ public class KnownElements {
                         }
                     }
                 } catch (Exception e1) {
-                    // ignore
+                    Logger.info(String.format(
+                            "An exception happened while restoring the cached element '%s'", id), e1);
                 }
                 if (ui2Object == null) {
                     throw new StaleElementReferenceException(String.format(
@@ -93,6 +110,5 @@ public class KnownElements {
             cache.clear();
             System.gc();
         }
-
     }
 }
