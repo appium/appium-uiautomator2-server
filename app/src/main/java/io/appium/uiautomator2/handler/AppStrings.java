16e0cbeb5b3d557273076177df9cd34f16213e22
Mykola Mokhnach <nick+1@wire.com>
2018-10-03 22:08:34 +0200

    Add logs

--- app/src/main/java/io/appium/uiautomator2/handler/AppStrings.java
+++ app/src/main/java/io/appium/uiautomator2/handler/AppStrings.java
@@ -1,13 +1,12 @@
 package io.appium.uiautomator2.handler;
 
-import org.json.JSONException;
-
 import java.util.Collections;
 
 import io.appium.uiautomator2.handler.request.SafeRequestHandler;
 import io.appium.uiautomator2.http.AppiumResponse;
 import io.appium.uiautomator2.http.IHttpRequest;
 import io.appium.uiautomator2.server.WDStatus;
+import io.appium.uiautomator2.utils.Logger;
 
 @Deprecated
 public class AppStrings extends SafeRequestHandler {
@@ -16,7 +15,9 @@ public class AppStrings extends SafeRequestHandler {
     }
 
     @Override
-    protected AppiumResponse safeHandle(IHttpRequest request) throws JSONException {
+    protected AppiumResponse safeHandle(IHttpRequest request) {
+        Logger.info("The /app/strings endpoint is deprecated " +
+                "and will be removed in a future release");
         return new AppiumResponse(getSessionId(request), WDStatus.SUCCESS, Collections.emptyMap());
     }
 }
