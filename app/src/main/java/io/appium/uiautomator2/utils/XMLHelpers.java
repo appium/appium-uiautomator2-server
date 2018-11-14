3262a9b6c714d9be7abc3f81c40e144697725b3f
Mykola Mokhnach <nick+1@wire.com>
2018-11-14 13:42:22 +0100

    Include underscore to the list of allowed tag chars

--- app/src/main/java/io/appium/uiautomator2/utils/XMLHelpers.java
+++ app/src/main/java/io/appium/uiautomator2/utils/XMLHelpers.java
@@ -28,12 +28,12 @@ public abstract class XMLHelpers {
     private final static Pattern XML10_PATTERN = Pattern.compile("[^" + "\u0009\r\n" +
             "\u0020-\uD7FF" + "\uE000-\uFFFD" + "\ud800\udc00-\udbff\udfff" + "]");
     // https://stackoverflow.com/questions/3158274/what-would-be-a-regex-for-valid-xml-names
-    private final static Pattern XML10_START_TAG_PATTERN = Pattern.compile("^[^" + ":_" +
+    private final static Pattern XML10_START_TAG_PATTERN = Pattern.compile("^[^" + "_" +
             "A-Z" + "a-z" + "\u00C0-\u00D6" + "\u00F8-\u02FF" +
             "\u200C-\u200D" + "\u2070-\u218F" + "\u2C00-\u2FEF" +
             "\u3001-\uD7FF" + "\uF900-\uFDCF" + "\uFDF0-\uFFFD" +
             "\ud800\udc00-\udbff\udfff" + "]+");
-    private final static Pattern XML10_TAG_PATTERN = Pattern.compile("[^" + "\\-." +
+    private final static Pattern XML10_TAG_PATTERN = Pattern.compile("[^" + "\\-._" +
             "0-9" + "\u00B7" + "\u0300-\u036F" + "\u203F-\u2040" +
             "A-Z" + "a-z" + "\u00C0-\u00D6" + "\u00F8-\u02FF" +
             "\u200C-\u200D" + "\u2070-\u218F" + "\u2C00-\u2FEF" +
