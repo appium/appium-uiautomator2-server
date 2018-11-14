3bcd56846d913218bf476a8882b73cf92375cf40
Mykola Mokhnach <nick+1@wire.com>
2018-11-14 10:24:32 +0100

    Switch to StAX (XMLPull) implementation for xml source building

--- app/src/main/java/io/appium/uiautomator2/utils/XMLHelpers.java
+++ app/src/main/java/io/appium/uiautomator2/utils/XMLHelpers.java
@@ -27,6 +27,18 @@ public abstract class XMLHelpers {
     // #x9 | #xA | #xD | [#x20-#xD7FF] | [#xE000-#xFFFD] | [#x10000-#x10FFFF]
     private final static Pattern XML10_PATTERN = Pattern.compile("[^" + "\u0009\r\n" +
             "\u0020-\uD7FF" + "\uE000-\uFFFD" + "\ud800\udc00-\udbff\udfff" + "]");
+    // https://stackoverflow.com/questions/3158274/what-would-be-a-regex-for-valid-xml-names
+    private final static Pattern XML10_START_TAG_PATTERN = Pattern.compile("^[^" + ":_" +
+            "A-Z" + "a-z" + "\u00C0-\u00D6" + "\u00F8-\u02FF" +
+            "\u200C-\u200D" + "\u2070-\u218F" + "\u2C00-\u2FEF" +
+            "\u3001-\uD7FF" + "\uF900-\uFDCF" + "\uFDF0-\uFFFD" +
+            "\ud800\udc00-\udbff\udfff" + "]+");
+    private final static Pattern XML10_TAG_PATTERN = Pattern.compile("[^" + "\\-." +
+            "0-9" + "\u00B7" + "\u0300-\u036F" + "\u203F-\u2040" +
+            "A-Z" + "a-z" + "\u00C0-\u00D6" + "\u00F8-\u02FF" +
+            "\u200C-\u200D" + "\u2070-\u218F" + "\u2C00-\u2FEF" +
+            "\u3001-\uD7FF" + "\uF900-\uFDCF" + "\uFDF0-\uFFFD" +
+            "\ud800\udc00-\udbff\udfff" + "]");
     public final static String DEFAULT_VIEW_CLASS_NAME = "android.view.View";
 
     public static String toNodeName(String className) {
@@ -38,25 +50,20 @@ public abstract class XMLHelpers {
                 .replaceAll("[$@#&]", ".")
                 .replaceAll("\\.+", ".")
                 .replaceAll("(^\\.|\\.$)", "");
-        //noinspection ConstantConditions
-        fixedName = toSafeXmlString(fixedName, "");
-        // Workaround for https://issuetracker.google.com/issues/37008591
-        fixedName = toAndroidXmlCompatibleNodeName(fixedName);
+        fixedName = XML10_START_TAG_PATTERN
+                .matcher(fixedName)
+                .replaceAll("");
+        fixedName = XML10_TAG_PATTERN
+                .matcher(fixedName)
+                .replaceAll("_");
         if (!fixedName.equals(className)) {
             Logger.info(String.format("Rewrote XML node name '%s' to '%s'", className, fixedName));
         }
         return StringUtils.isBlank(fixedName) ? DEFAULT_VIEW_CLASS_NAME : fixedName;
     }
 
-    private static String toAndroidXmlCompatibleNodeName(String sourceStr) {
-        return Pattern.compile("[^A-Za-z\\-._]")
-                .matcher(sourceStr)
-                .replaceAll("_")
-                .replaceAll("^-+", "");
-    }
-
     @Nullable
-    public static String toSafeXmlString(Object source, String replacement) {
+    public static String toSafeString(Object source, String replacement) {
         return source == null ? null : XML10_PATTERN
                 .matcher(String.valueOf(source))
                 .replaceAll(replacement);
