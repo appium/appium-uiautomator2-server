ca6d6816f7fa7a7f08e41365d373508c59c8960d
Mykola Mokhnach <mokhnach@gmail.com>
2019-02-28 19:18:59 +0100

    Update session

--- app/src/main/java/io/appium/uiautomator2/model/Session.java
+++ app/src/main/java/io/appium/uiautomator2/model/Session.java
@@ -1,3 +1,19 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.appium.uiautomator2.model;
 
 import java.util.Collections;
@@ -11,7 +27,7 @@ import static io.appium.uiautomator2.model.settings.Settings.SHOULD_USE_COMPACT_
 
 public class Session {
     public static final String NO_ID = "None";
-    public final Map<String, Object> capabilities = new HashMap<>();
+    private final Map<String, Object> capabilities = new HashMap<>();
     private final String sessionId;
     private final KnownElements knownElements = new KnownElements();
     private AccessibilityScrollData lastScrollData;
@@ -47,7 +63,8 @@ public class Session {
 
     public boolean shouldUseCompactResponses() {
         String capName = SHOULD_USE_COMPACT_RESPONSES.toString();
-        return !hasCapability(capName) || String.valueOf(getCapability(capName)).equals("true");
+        return !hasCapability(capName)
+                || String.valueOf(getCapability(capName)).equalsIgnoreCase("true");
     }
 
     public String[] getElementResponseAttributes() {
