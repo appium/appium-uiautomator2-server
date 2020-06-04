07cc56da3870c2fdfe4de69ffbea300f08335a6b
Mykola Mokhnach <mokhnach@gmail.com>
2020-06-04 22:08:06 +0200

    fix: Limit the maximum keep-alive timeout for a single channel

---
+++ app/src/main/java/io/appium/uiautomator2/http/KeepAliveStateHandler.java
@@ -0,0 +1,24 @@
+package io.appium.uiautomator2.http;
+
+import io.appium.uiautomator2.utils.Logger;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.timeout.IdleState;
+import io.netty.handler.timeout.IdleStateEvent;
+import io.netty.handler.timeout.IdleStateHandler;
+
+public class KeepAliveStateHandler extends IdleStateHandler {
+    private static final int MAX_IDLE_TIME_SEC = 60;
+
+    public KeepAliveStateHandler() {
+        super(MAX_IDLE_TIME_SEC, MAX_IDLE_TIME_SEC, MAX_IDLE_TIME_SEC);
+    }
+
+    @Override
+    public void channelIdle(ChannelHandlerContext ctx, IdleStateEvent e) {
+        Logger.debug(e.state().name());
+        if (e.state() == IdleState.READER_IDLE) {
+            Logger.info("closing the channel");
+            ctx.close();
+        }
+    }
+}
