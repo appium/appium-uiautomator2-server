312ec0be75128554fdaa3936a278df1e6d0422ff
Mykola Mokhnach <mokhnach@gmail.com>
2020-06-05 07:31:49 +0200

    Tune logging

--- app/src/main/java/io/appium/uiautomator2/http/KeepAliveStateHandler.java
+++ app/src/main/java/io/appium/uiautomator2/http/KeepAliveStateHandler.java
@@ -10,14 +10,13 @@ public class KeepAliveStateHandler extends IdleStateHandler {
     private static final int MAX_IDLE_TIME_SEC = 60;
 
     public KeepAliveStateHandler() {
-        super(MAX_IDLE_TIME_SEC, MAX_IDLE_TIME_SEC, MAX_IDLE_TIME_SEC);
+        super(MAX_IDLE_TIME_SEC, 0, 0);
     }
 
     @Override
     public void channelIdle(ChannelHandlerContext ctx, IdleStateEvent e) {
-        Logger.debug(e.state().name());
         if (e.state() == IdleState.READER_IDLE) {
-            Logger.info("closing the channel");
+            Logger.info(String.format("%s: closing the channel", e.state().name()));
             ctx.close();
         }
     }
