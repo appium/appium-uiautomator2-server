f8933220c9fab10e7b992dc3d06839cebf2ed98c
Mykola Mokhnach <mokhnach@gmail.com>
2022-08-28 21:11:43 +0200

    Filter .apk files

--- scripts/sign-apk.js
+++ scripts/sign-apk.js
@@ -8,7 +8,8 @@ async function signApks () {
   // Signs the APK with the default Appium Certificate
   const adb = new ADB();
   const apksRoot = path.resolve(__dirname, '..', 'apks');
-  const apks = await fs.promises.readdir(apksRoot);
+  const apks = (await fs.promises.readdir(apksRoot))
+    .filter((name) => path.extname(name) === '.apk');
   if (!apks.length) {
     throw new Error(`There are no .apk files available for signing in '${apksRoot}'`);
   }
