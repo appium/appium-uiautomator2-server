9b9030121f408065ab8ff13c1491ee0c2330dbc7
Mykola Mokhnach <mokhnach@gmail.com>
2025-12-01 09:52:43 +0100

    feat: Add additional properties to listWindows output

--- app/src/main/java/io/appium/uiautomator2/model/api/WindowModel.java
+++ app/src/main/java/io/appium/uiautomator2/model/api/WindowModel.java
@@ -21,7 +21,7 @@ import androidx.annotation.Nullable;
 
 public class WindowModel {
     public Integer windowId;
-    public Integer displayId;
+    public int displayId;
     @Nullable
     public Long physicalDisplayId;
     public ElementRectModel rect;
@@ -29,12 +29,19 @@ public class WindowModel {
     public String packageName;
     @Nullable
     public String screenshot;
+    public int type;
+    @Nullable
+    public String title;
+    public boolean isAccessibilityFocused;
+    public boolean isActive;
+    public boolean isFocused;
+    public boolean isInPictureInPictureMode;
 
     public WindowModel() {}
 
     public WindowModel(
         Integer windowId,
-        Integer displayId,
+        int displayId,
         @Nullable Long physicalDisplayId,
         Rect rect,
         @Nullable String packageName,
@@ -46,6 +53,34 @@ public class WindowModel {
         this.rect = new ElementRectModel(rect);
         this.packageName = packageName;
         this.screenshot = screenshot;
+    }
+
+    public WindowModel(
+        Integer windowId,
+        int displayId,
+        @Nullable Long physicalDisplayId,
+        Rect rect,
+        @Nullable String packageName,
+        @Nullable String screenshot,
+        int type,
+        @Nullable String title,
+        boolean isAccessibilityFocused,
+        boolean isActive,
+        boolean isFocused,
+        boolean isInPictureInPictureMode
+    ) {
+        this.windowId = windowId;
+        this.displayId = displayId;
+        this.physicalDisplayId = physicalDisplayId;
+        this.rect = new ElementRectModel(rect);
+        this.packageName = packageName;
+        this.screenshot = screenshot;
+        this.type = type;
+        this.title = title;
+        this.isAccessibilityFocused = isAccessibilityFocused;
+        this.isActive = isActive;
+        this.isFocused = isFocused;
+        this.isInPictureInPictureMode = isInPictureInPictureMode;
     }
 }
 
