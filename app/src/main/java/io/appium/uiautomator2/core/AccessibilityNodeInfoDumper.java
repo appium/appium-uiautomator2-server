abdul-sathar/Appium-UI-Automator-Server

commit b4b655ec14195977903476cbc81a99287b79c877

Refactor source XML tree generation (#208)

app/src/main/java/io/appium/uiautomator2/core/AccessibilityNodeInfoDumper.java
@@ -13,213 +13,143 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.appium.uiautomator2.core;
 
 import android.graphics.Point;
 import android.os.SystemClock;
-import android.util.Xml;
+import android.support.annotation.Nullable;
+import android.util.SparseArray;
 import android.view.Display;
 import android.view.accessibility.AccessibilityNodeInfo;
 
-import org.xmlpull.v1.XmlSerializer;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
-import java.io.IOException;
-import java.io.StringWriter;
+import java.lang.reflect.Field;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import io.appium.uiautomator2.common.exceptions.UiAutomator2Exception;
+import io.appium.uiautomator2.model.NotificationListener;
+import io.appium.uiautomator2.model.UiAutomationElement;
+import io.appium.uiautomator2.model.UiElement;
+import io.appium.uiautomator2.utils.Attribute;
 import io.appium.uiautomator2.utils.Logger;
 
-import static io.appium.uiautomator2.utils.XMLHierarchy.safeCharSeqToString;
+import static io.appium.uiautomator2.utils.AXWindowHelpers.currentActiveWindowRoot;
+import static io.appium.uiautomator2.utils.XMLHelpers.DEFAULT_VIEW_CLASS_NAME;
+import static io.appium.uiautomator2.utils.XMLHelpers.toNodeName;
+import static io.appium.uiautomator2.utils.XMLHelpers.toSafeXmlString;
 
-
-/**
- * The AccessibilityNodeInfoDumper in Android Open Source Project contains a lot of bugs which will
- * stay in old android versions forever. By coping the code of the latest version it is ensured that
- * all patches become available on old android versions. <p/> down ported bugs are e.g. { @link
- * https://code.google.com/p/android/issues/detail?id=62906 } { @link
- * https://code.google.com/p/android/issues/detail?id=58733 }
- */
 public class AccessibilityNodeInfoDumper {
-    private static final String[] NAF_EXCLUDED_CLASSES = new String[]{
-            android.widget.GridView.class.getName(),
-            android.widget.GridLayout.class.getName(),
-            android.widget.ListView.class.getName(),
-            android.widget.TableLayout.class.getName()};
     // https://github.com/appium/appium/issues/10204
     private static final int MAX_DEPTH = 70;
+    private static final String UI_ELEMENT_INDEX = "uiElementIndex";
 
-    /**
-     * Using {@link AccessibilityNodeInfo} this method will walk the layout hierarchy and return
-     * String object of xml hierarchy
-     *
-     * @param root The root accessibility node.
-     */
-    public static String getWindowXMLHierarchy(AccessibilityNodeInfo root) {
-        final long startTime = SystemClock.uptimeMillis();
-        StringWriter xmlDump = new StringWriter();
+    private static void setNodeLocalName(Element element, String className) {
         try {
-            XmlSerializer serializer = Xml.newSerializer();
-            serializer.setOutput(xmlDump);
-            serializer.startDocument("UTF-8", true);
-            serializer.startTag("", "hierarchy");
-
-            if (root != null) {
-                Display display = UiAutomatorBridge.getInstance().getDefaultDisplay();
-                Point size = new Point();
-                display.getSize(size);
-                final int width = size.x;
-                final int height = size.y;
-
-                serializer.attribute("", "rotation", Integer.toString(display.getRotation()));
-
-                dumpNodeRec(root, serializer, 0, width, height, 0);
-            }
-
-            serializer.endTag("", "hierarchy");
-            serializer.endDocument();
-
-            /*FileWriter writer = new FileWriter(dumpFile);
-            writer.write(stringWriter.toString());
-            writer.close();*/
-        } catch (IOException e) {
-            throw new UiAutomator2Exception("Cannot dump views hierarchy to XML format", e);
+            Field localName = element.getClass().getDeclaredField("localName");
+            localName.setAccessible(true);
+            localName.set(element, tag(className));
+        } catch (Exception e) {
+            Logger.error(String.format("Unable to set field's localName to '%s'", className), e);
         }
-        final long endTime = SystemClock.uptimeMillis();
-        Logger.info("Fetch time: " + (endTime - startTime) + "ms");
-        return xmlDump.toString();
     }
-    
-    private static void dumpNodeRec(AccessibilityNodeInfo node, XmlSerializer serializer,
-                                    int index, int width, int height, final int depth)
-            throws IOException {
-        // Some views might have unlimited number of children:
-        // https://bugs.chromium.org/p/chromium/issues/detail?id=805014
+
+    private static Element toDOMElement(UiElement<?, ?> uiElement, final Document document,
+                                        final SparseArray<UiElement<?, ?>> uiElementsMapping,
+                                        final int depth) {
+        String className = uiElement.getClassName();
+        if (className == null) {
+            className = DEFAULT_VIEW_CLASS_NAME;
+        }
+        Element element = document.createElement(toNodeName(className));
+        final int uiElementIndex = uiElementsMapping.size();
+        uiElementsMapping.put(uiElementIndex, uiElement);
+
+        /*
+         * Setting the Element's className field.
+         * Reason for setting className field in Element object explicitly,
+         * className property might contain special characters like '$' if it is a Inner class and
+         * just not possible to create Element object with special characters.
+         * But Appium should consider Inner classes i.e special characters should be included.
+         */
+        setNodeLocalName(element, className);
+
+        for (Attribute attr : Attribute.values()) {
+            setAttribute(element, attr, toSafeXmlString(uiElement.get(attr), "?"));
+        }
+        element.setAttribute(UI_ELEMENT_INDEX, Integer.toString(uiElementIndex));
+
         if (depth >= MAX_DEPTH) {
             Logger.error(String.format("The xml tree dump has reached its maximum depth of %s at " +
-                            "%s. The recursion is stopped to avoid StackOverflowError", MAX_DEPTH,
-                    node.toString()));
-            return;
-        }
-
-        serializer.startTag("", "node");
-        if (!isOfNafExcludedClass(node) && !isAccessibilityFriendly(node))
-            serializer.attribute("", "NAF", Boolean.toString(true));
-        serializer.attribute("", "index", Integer.toString(index));
-        final String text;
-        if (node.getRangeInfo() == null) {
-            text = safeCharSeqToString(node.getText());
+                    "'%s'. The recursion is stopped to avoid StackOverflowError", MAX_DEPTH, className));
         } else {
-            text = Float.toString(node.getRangeInfo().getCurrent());
-        }
-        serializer.attribute("", "text", text);
-        serializer.attribute("", "class", safeCharSeqToString(node.getClassName()));
-        serializer.attribute("", "package", safeCharSeqToString(node.getPackageName()));
-        serializer.attribute("", "content-desc", safeCharSeqToString(node.getContentDescription()));
-        serializer.attribute("", "checkable", Boolean.toString(node.isCheckable()));
-        serializer.attribute("", "checked", Boolean.toString(node.isChecked()));
-        serializer.attribute("", "clickable", Boolean.toString(node.isClickable()));
-        serializer.attribute("", "enabled", Boolean.toString(node.isEnabled()));
-        serializer.attribute("", "focusable", Boolean.toString(node.isFocusable()));
-        serializer.attribute("", "focused", Boolean.toString(node.isFocused()));
-        serializer.attribute("", "scrollable", Boolean.toString(node.isScrollable()));
-        serializer.attribute("", "long-clickable", Boolean.toString(node.isLongClickable()));
-        serializer.attribute("", "password", Boolean.toString(node.isPassword()));
-        serializer.attribute("", "selected", Boolean.toString(node.isSelected()));
-        serializer.attribute("", "bounds",
-                AccessibilityNodeInfoHelper.getVisibleBoundsInScreen(node, width, height).toShortString());
-        serializer.attribute("", "resource-id", safeCharSeqToString(node.getViewIdResourceName()));
-
-        int count = node.getChildCount();
-        for (int i = 0; i < count; i++) {
-            AccessibilityNodeInfo child = node.getChild(i);
-            if (child != null) {
-                if (child.isVisibleToUser()) {
-                    dumpNodeRec(child, serializer, i, width, height, depth + 1);
-                    child.recycle();
-                } else {
-                    Logger.info(String.format("Skipping invisible child: %s", child.toString()));
-                }
-            } else {
-                Logger.info(String.format("Null child %s/%s, parent: %s", i, count, node.toString()));
+            for (UiElement<?, ?> child : uiElement.getChildren()) {
+                element.appendChild(toDOMElement(child, document, uiElementsMapping, depth + 1));
             }
         }
-        serializer.endTag("", "node");
+        return element;
+    }
+
+    private static void setAttribute(Element element, Attribute attr, Object value) {
+        if (value != null) {
+            element.setAttribute(attr.getName(), String.valueOf(value));
+        }
+    }
+
+    public static Document asXmlDocument() {
+        return asXmlDocument(null, null);
+    }
+
+    public static Document asXmlDocument(@Nullable AccessibilityNodeInfo root,
+                                         @Nullable SparseArray<UiElement<?, ?>> uiElementsMapping) {
+        final long startTime = SystemClock.uptimeMillis();
+        final Document document;
+        try {
+            document = DocumentBuilderFactory.newInstance()
+                    .newDocumentBuilder()
+                    .newDocument();
+        } catch (ParserConfigurationException e) {
+            throw new UiAutomator2Exception(e);
+        }
+        if (uiElementsMapping == null) {
+            uiElementsMapping = new SparseArray<>();
+        }
+        final UiElement<?, ?> xpathRoot = root == null
+                ? UiAutomationElement.rebuildForNewRoot(currentActiveWindowRoot(), NotificationListener
+                .getInstance().getToastMessage())
+                : UiAutomationElement.rebuildForNewRoot(root, null);
+        final Element domNode = toDOMElement(xpathRoot, document, uiElementsMapping, 0);
+        if (root == null) {
+            alterDisplayInfo(domNode);
+        }
+        document.appendChild(domNode);
+        Logger.info(String.format("XML tree fetch time: %sms", SystemClock.uptimeMillis() - startTime));
+        return document;
+    }
+
+    private static void alterDisplayInfo(Element node) {
+        Display display = UiAutomatorBridge.getInstance().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        node.setAttribute("rotation", Integer.toString(display.getRotation()));
+        node.setAttribute("width", Integer.toString(size.x));
+        node.setAttribute("height", Integer.toString(size.y));
     }
 
     /**
-     * The list of classes to exclude may not be complete. We're attempting to only reduce noise from
-     * standard layout classes that may be falsely configured to accept clicks and are also
-     * enabled.
-     *
-     * @return true if node is excluded.
+     * @param clsName the original class name
+     * @return The tag name used to build UiElement DOM. It is preferable to use
+     * this to build XPath instead of String literals.
      */
-    private static boolean isOfNafExcludedClass(AccessibilityNodeInfo node) {
-        String className = safeCharSeqToString(node.getClassName());
-        for (String excludedClassName : NAF_EXCLUDED_CLASSES) {
-            if (className.endsWith(excludedClassName)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * We're looking for UI controls that are enabled, clickable but have no text nor
-     * content-description. Such controls configuration indicate an interactive control is present
-     * in the UI and is most likely not accessibility friendly. We refer to such controls here as
-     * NAF controls (Not Accessibility Friendly)
-     *
-     * @return false if a node fails the check, true if all is OK
-     */
-    private static boolean isAccessibilityFriendly(AccessibilityNodeInfo node) {
-        boolean isNaf = node.isClickable() && node.isEnabled() &&
-                safeCharSeqToString(node.getContentDescription()).isEmpty() &&
-                safeCharSeqToString(node.getText()).isEmpty();
-        if (!isNaf) {
-            return true;
-        }
-        // check children since sometimes the containing element is clickable
-        // and NAF but a child's text or description is available. Will assume
-        // such layout as fine.
-        return isAnyDescendantAccessibilityFriendly(node, 0);
-    }
-
-    /**
-     * This should be used when it's already determined that the node is NAF and a further check of
-     * its children is in order. A node maybe a container such as LinerLayout and may be set to be
-     * clickable but have no text or content description but it is counting on one of its children
-     * to fulfill the requirement for being accessibility friendly by having one or more of its
-     * children fill the text or content-description. Such a combination is considered by this
-     * dumper as acceptable for accessibility.
-     *
-     * @return false if node fails the check.
-     */
-    private static boolean isAnyDescendantAccessibilityFriendly(AccessibilityNodeInfo node,
-                                                                final int depth) {
-        // Some views might have unlimited number of children:
-        // https://bugs.chromium.org/p/chromium/issues/detail?id=805014
-        if (depth >= MAX_DEPTH) {
-            Logger.error(String.format("The NAF verification has reached its maximum depth of %s at " +
-                            "%s. The recursion is stopped to avoid StackOverflowError", MAX_DEPTH,
-                    node.toString()));
-            return false;
-        }
-
-        int childCount = node.getChildCount();
-        for (int x = 0; x < childCount; x++) {
-            AccessibilityNodeInfo childNode = node.getChild(x);
-            if (childNode == null) {
-                Logger.info(String.format("Null child %s/%s, parent: %s", x, childCount, node.toString()));
-                continue;
-            }
-            if (!safeCharSeqToString(childNode.getContentDescription()).isEmpty()
-                    || !safeCharSeqToString(childNode.getText()).isEmpty()) {
-                return true;
-            }
-            if (isAnyDescendantAccessibilityFriendly(childNode, depth + 1)) {
-                return true;
-            }
-        }
-        return false;
+    private static String tag(String clsName) {
+        // the nth anonymous class has a class name ending in "Outer$n"
+        // and local inner classes have names ending in "Outer.$1Inner"
+        return clsName
+                .replaceAll("\\?+", "_")
+                .replaceAll("\\$[0-9]+", "\\$");
     }
 }

appium/appium-uiautomator2-server

commit 2534cd5194f2cd9ba79d23a79dabd41769f161ed

fix: Recycle AccessibilityNodeInfo/Window instances to stop native memory leak
The in-memory UiElementSnapshot tree introduced in #208 dropped the per-node
recycle() the previous streaming dumper performed, leaking one
AccessibilityNodeInfo per visited node (plus transient window/parent infos) on
every page source / find query. On API < 33 these are pooled native resources,
so long polling-style runs grow the native heap unbounded until the
lowmemorykiller terminates the server (see appium/appium-uiautomator2-server#308).

- AccessibilityNodeInfoDumper: recycle the snapshot tree after each dump/find,
  retaining only the cached window roots and the matched nodes handed back to
  the client.
- AxNodeInfoHelper: recycle the transient getWindow()/getParent()/getChild()
  instances created in getBounds, getVisibleBoundsInScreen, getAxNodeDisplayId
  and calculateIndex.

All recycling is guarded for SDK_INT < TIRAMISU (no-op where the framework
manages node lifecycle) and tolerates already-recycled instances.

Co-Authored-By: Claude Opus 4.8 (1M context) <noreply@anthropic.com>

app/src/main/java/io/appium/uiautomator2/core/AccessibilityNodeInfoDumper.java
@@ -17,6 +17,7 @@
 package io.appium.uiautomator2.core;
 
 import android.graphics.Point;
+import android.os.Build;
 import android.os.SystemClock;
 import android.util.SparseArray;
 import android.util.Xml;
@@ -48,6 +49,10 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.IdentityHashMap;
 import java.util.Iterator;
 import java.util.Objects;
 import java.util.Set;
@@ -95,6 +100,14 @@ public class AccessibilityNodeInfoDumper {
     private final Set<Attribute> includedAttributes;
     private boolean shouldAddDisplayInfo;
     private XmlSerializer serializer;
+    // The most recently built source snapshot tree, so its transient nodes can be recycled
+    // once the tree has been fully consumed (serialized and/or matched against).
+    @Nullable
+    private UiElement<?, ?> snapshotRoot;
+    // Externally-owned nodes (cached window roots or the explicit context root) that are reused
+    // across queries and therefore must not be recycled together with the snapshot tree.
+    private final Set<AccessibilityNodeInfo> retainedNodes =
+            Collections.newSetFromMap(new IdentityHashMap<>());
 
     public AccessibilityNodeInfoDumper(@Nullable AccessibilityNodeInfo root,
                                        Set<Attribute> includedAttributes) {
@@ -222,17 +235,67 @@ public class AccessibilityNodeInfoDumper {
             serializer.setOutput(outputStream, XML_ENCODING);
             serializer.startDocument(XML_ENCODING, true);
             serializer.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true);
-            UiElement<?, ?> uiRootElement = root != null && Settings.get(LimitXpathContextScope.class).getValue()
-                    ? UiElementSnapshot.take(root, includedAttributes)
-                    : UiElementSnapshot.take(
-                        getCachedWindowRoots(), NotificationListener.getInstance().getToastMessage(),
+            final UiElement<?, ?> uiRootElement;
+            if (root != null && Settings.get(LimitXpathContextScope.class).getValue()) {
+                uiRootElement = UiElementSnapshot.take(root, includedAttributes);
+                retainedNodes.add(root);
+            } else {
+                AccessibilityNodeInfo[] windowRoots = getCachedWindowRoots();
+                uiRootElement = UiElementSnapshot.take(
+                        windowRoots, NotificationListener.getInstance().getToastMessage(),
                         includedAttributes
-                    );
+                );
+                retainedNodes.addAll(Arrays.asList(windowRoots));
+            }
+            snapshotRoot = uiRootElement;
             serializeUiElement(uiRootElement, isIndexed);
             serializer.endDocument();
             Logger.debug(String.format("The source XML tree (%s bytes) has been fetched in %sms",
                     outputStream.size(), SystemClock.uptimeMillis() - startTime));
             return new ByteArrayInputStream(outputStream.toByteArray());
+        }
+    }
+
+    /**
+     * Recycles every {@link AccessibilityNodeInfo} that was created while building the source
+     * snapshot tree, except for the externally-owned roots ({@link #retainedNodes}) and any nodes
+     * the caller still needs (e.g. nodes that matched a locator and are handed back to the client).
+     * <p>
+     * This reverses the per-node recycling that existed before the streaming dumper was replaced
+     * with an in-memory snapshot tree (#208), and which had been silently leaking one
+     * {@code AccessibilityNodeInfo} per visited node on every page source / find query. It is a
+     * no-op on API 33+, where these instances are managed by the framework.
+     */
+    private void recycleSnapshotNodes(@Nullable Collection<AccessibilityNodeInfo> retainAdditionally) {
+        try {
+            if (snapshotRoot == null || Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                return;
+            }
+            Set<AccessibilityNodeInfo> keep = Collections.newSetFromMap(new IdentityHashMap<>());
+            keep.addAll(retainedNodes);
+            if (retainAdditionally != null) {
+                keep.addAll(retainAdditionally);
+            }
+            recycleTree(snapshotRoot, keep);
+        } catch (Exception e) {
+            Logger.warn("Failed to recycle the source snapshot nodes", e);
+        } finally {
+            snapshotRoot = null;
+            retainedNodes.clear();
+        }
+    }
+
+    private static void recycleTree(UiElement<?, ?> element, Set<AccessibilityNodeInfo> keep) {
+        for (UiElement<?, ?> child : element.getChildren()) {
+            recycleTree(child, keep);
+        }
+        AccessibilityNodeInfo node = element.getNode();
+        if (node != null && !keep.contains(node)) {
+            try {
+                node.recycle();
+            } catch (IllegalStateException e) {
+                // The node has already been recycled - ignore
+            }
         }
     }
 
@@ -247,6 +310,7 @@ public class AccessibilityNodeInfoDumper {
         } catch (IOException e) {
             throw new UiAutomator2Exception(e);
         } finally {
+            recycleSnapshotNodes(null);
             uiElementsMapping.clear();
             RESOURCES_GUARD.release();
         }
@@ -276,11 +340,11 @@ public class AccessibilityNodeInfoDumper {
         } catch (InterruptedException e) {
             throw new UiAutomator2Exception(e);
         }
+        final NodeInfoList matchedNodes = new NodeInfoList();
         try (InputStream xmlStream = toStream(true)) {
             NodeList elements = (NodeList) expression.evaluate(
                     fetchContext(xmlStream), XPathConstants.NODESET
             );
-            final NodeInfoList matchedNodes = new NodeInfoList();
             final long timeStarted = SystemClock.uptimeMillis();
             IntStream.range(0, elements.getLength())
                     .mapToObj(elements::item)
@@ -305,6 +369,7 @@ public class AccessibilityNodeInfoDumper {
             Logger.error("Got an unexpected error while fetching matches using XPath1", e);
             throw new UiAutomator2Exception(e);
         } finally {
+            recycleSnapshotNodes(matchedNodes.getAll());
             uiElementsMapping.clear();
             RESOURCES_GUARD.release();
         }
@@ -324,11 +389,11 @@ public class AccessibilityNodeInfoDumper {
         } catch (InterruptedException e) {
             throw new UiAutomator2Exception(e);
         }
+        final NodeInfoList matchedNodes = new NodeInfoList();
         try (InputStream xmlStream = toStream(true)) {
             ResultSequence rs = expr.evaluate(
                     new DynamicContextBuilder(scb), new Object[]{fetchContext(xmlStream)}
             );
-            NodeInfoList matchedNodes = new NodeInfoList();
             Iterator<Item> iterator = rs.iterator();
             final long timeStarted = SystemClock.uptimeMillis();
             while (iterator.hasNext()) {
@@ -367,6 +432,7 @@ public class AccessibilityNodeInfoDumper {
                                     "to workaround the problem.", e.getMessage(),
                             Settings.ENFORCE_XPATH1.getSetting().getName()), e);
         } finally {
+            recycleSnapshotNodes(matchedNodes.getAll());
             uiElementsMapping.clear();
             RESOURCES_GUARD.release();
         }

app/src/main/java/io/appium/uiautomator2/core/AxNodeInfoHelper.java
@@ -38,8 +38,6 @@ import io.appium.uiautomator2.model.settings.SimpleBoundsCalculation;
 import io.appium.uiautomator2.model.settings.SnapshotMaxDepth;
 import io.appium.uiautomator2.utils.Logger;
 
-import java.util.stream.IntStream;
-
 import static io.appium.uiautomator2.utils.ReflectionUtils.getField;
 import static io.appium.uiautomator2.utils.StringHelpers.charSequenceToNullableString;
 import static io.appium.uiautomator2.utils.StringHelpers.charSequenceToString;
@@ -167,10 +165,39 @@ public class AxNodeInfoHelper {
         if (node != null && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             AccessibilityWindowInfo window = node.getWindow();
             if (window != null) {
-                return window.getDisplayId();
+                try {
+                    return window.getDisplayId();
+                } finally {
+                    recycleSafely(window);
+                }
             }
         }
         return Display.DEFAULT_DISPLAY;
+    }
+
+    /**
+     * Recycles a transient {@link AccessibilityNodeInfo} that does not escape the current scope.
+     * No-op on API 33+, where these instances are managed by the framework, and tolerant of
+     * nodes that have already been recycled.
+     */
+    public static void recycleSafely(@Nullable AccessibilityNodeInfo node) {
+        if (node != null && Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            try {
+                node.recycle();
+            } catch (IllegalStateException e) {
+                // The node has already been recycled - ignore
+            }
+        }
+    }
+
+    private static void recycleSafely(@Nullable AccessibilityWindowInfo window) {
+        if (window != null && Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+            try {
+                window.recycle();
+            } catch (IllegalStateException e) {
+                // The window has already been recycled - ignore
+            }
+        }
     }
 
     public static void pinchOpen(AccessibilityNodeInfo node, float percent) {
@@ -250,26 +277,31 @@ public class AxNodeInfoHelper {
         Rect bounds = new Rect();
         AccessibilityWindowInfo window = node.getWindow();
         if (window != null) {
-            window.getBoundsInScreen(bounds);
-            nodeRect.intersect(bounds);
+            try {
+                window.getBoundsInScreen(bounds);
+                nodeRect.intersect(bounds);
+            } finally {
+                recycleSafely(window);
+            }
         }
 
         // Trim the bounds into any scrollable ancestor, if required.
         if (trimScrollableParent) {
-            for (AccessibilityNodeInfo ancestor = node.getParent();
-                 ancestor != null;
-                 ancestor = ancestor.getParent()
-            ) {
+            AccessibilityNodeInfo ancestor = node.getParent();
+            while (ancestor != null) {
                 if (ancestor.isScrollable()) {
-                    if (depth >= Settings.get(SnapshotMaxDepth.class).getValue()) {
-                        break;
+                    if (depth < Settings.get(SnapshotMaxDepth.class).getValue()) {
+                        Rect ancestorRect = getVisibleBoundsInScreen(
+                                ancestor, displayRect, true, depth + 1
+                        );
+                        nodeRect.intersect(ancestorRect);
                     }
-                    Rect ancestorRect = getVisibleBoundsInScreen(
-                            ancestor, displayRect, true, depth + 1
-                    );
-                    nodeRect.intersect(ancestorRect);
+                    recycleSafely(ancestor);
                     break;
                 }
+                AccessibilityNodeInfo parent = ancestor.getParent();
+                recycleSafely(ancestor);
+                ancestor = parent;
             }
         }
 
@@ -281,10 +313,25 @@ public class AxNodeInfoHelper {
         if (parent == null) {
             return 0;
         }
-        return IntStream.range(0, parent.getChildCount())
-                .filter(index -> node.equals(parent.getChild(index)))
-                .findFirst()
-                .orElse(0);
+        try {
+            final int childCount = parent.getChildCount();
+            for (int index = 0; index < childCount; index++) {
+                AccessibilityNodeInfo child = parent.getChild(index);
+                if (child == null) {
+                    continue;
+                }
+                try {
+                    if (node.equals(child)) {
+                        return index;
+                    }
+                } finally {
+                    recycleSafely(child);
+                }
+            }
+            return 0;
+        } finally {
+            recycleSafely(parent);
+        }
     }
 
     /**